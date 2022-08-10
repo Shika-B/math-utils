e601a5e36c6bbed035657a4c99362391c9570332
Shika <abdelzighem10@gmail.com>
2022-08-10 04:16:35 +0200

    Add a few more tests, assignement operators and some macros for ref implementations

--- src/ring/mod.rs
+++ src/ring/mod.rs
@@ -29,15 +29,103 @@ pub trait Ring<'a>:
 where
     Self: 'a,
 {
+    fn is_field(&self) -> bool {
+        false
+    }
+    fn is_commutative(&self) -> bool {
+        false
+    }
+    fn inverse(&self) -> Option<Self> {
+        None
+    }
 }
 
-impl<'a> Ring<'a> for i8 {}
-impl<'a> Ring<'a> for i16 {}
-impl<'a> Ring<'a> for i32 {}
-impl<'a> Ring<'a> for i64 {}
-impl<'a> Ring<'a> for i128 {}
-impl<'a> Ring<'a> for isize {}
+impl<'a> Ring<'a> for i8 {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+}
 
+impl<'a> Ring<'a> for i16 {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+}
+
+impl<'a> Ring<'a> for i32 {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+}
+
+impl<'a> Ring<'a> for i64 {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+}
+
+impl<'a> Ring<'a> for i128 {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+}
+
+impl<'a> Ring<'a> for isize {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+}
+
+impl<'a> Ring<'a> for Rational32 {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+    fn is_field(&self) -> bool {
+        true
+    }
+}
+impl<'a> Ring<'a> for Rational64 {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+    fn is_field(&self) -> bool {
+        true
+    }
+}
+
+impl<'a> Ring<'a> for f32 {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+    fn is_field(&self) -> bool {
+        true
+    }
+}
+impl<'a> Ring<'a> for f64 {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+    fn is_field(&self) -> bool {
+        true
+    }
+}
+
+impl<'a> Ring<'a> for BigInt {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+}
+
+impl<'a> Ring<'a> for BigRational {
+    fn is_commutative(&self) -> bool {
+        true
+    }
+    fn is_field(&self) -> bool {
+        true
+    }
+}
+
+// impl<'a> Ring<'a> for BigUint {}
 /*
 impl<'a> Ring<'a> for u8 {}
 impl<'a> Ring<'a> for u16 {}
@@ -46,13 +134,3 @@ impl<'a> Ring<'a> for u64 {}
 impl<'a> Ring<'a> for u128 {}
 impl<'a> Ring<'a> for usize {}
 */
-
-impl<'a> Ring<'a> for Rational32 {}
-impl<'a> Ring<'a> for Rational64 {}
-
-impl<'a> Ring<'a> for f32 {}
-impl<'a> Ring<'a> for f64 {}
-
-impl<'a> Ring<'a> for BigInt {}
-// impl<'a> Ring<'a> for BigUint {}
-impl<'a> Ring<'a> for BigRational {}
--- src/ring/polynomial/univar_polynomial.rs
+++ src/ring/polynomial/univar_polynomial.rs
@@ -1,7 +1,7 @@
 use std::{
     fmt::Debug,
     marker::PhantomData,
-    ops::{Add, Mul, Sub},
+    ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign},
 };
 
 use num::{pow, One, Zero};
@@ -98,6 +98,30 @@ impl<'a, R: Ring<'a>> Mul<Self> for UPolynomial<'a, R> {
     }
 }
 
+impl<'a, R: Ring<'a>> AddAssign<Self> for UPolynomial<'a, R> {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = self.clone() + rhs;
+    }
+}
+
+impl<'a, R: Ring<'a>> SubAssign<Self> for UPolynomial<'a, R> {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = self.clone() - rhs;
+    }
+}
+impl<'a, R: Ring<'a>> MulAssign<Self> for UPolynomial<'a, R> {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = self.clone() * rhs;
+    }
+}
+
+impl<'a, R: Ring<'a>> Neg for UPolynomial<'a, R> {
+    type Output = Self;
+    fn neg(self) -> Self {
+        self.scale(-R::one())
+    }
+}
+
 impl<'a, R: Ring<'a>> Zero for UPolynomial<'a, R> {
     fn zero() -> Self {
         Self {
@@ -124,33 +148,35 @@ impl<'a, R: Ring<'a>> One for UPolynomial<'a, R> {
     }
 }
 
-impl<'a, R: Ring<'a>> Add<&'a Self> for UPolynomial<'a, R> {
-    type Output = Self;
-
-    fn add(self, other: &'a Self) -> Self {
-        let other = other.clone();
-        self + other
-    }
+macro_rules! ref_bin_op {
+    ($trait:tt, $method_name:ident) => {
+        impl<'a, R: Ring<'a>> $trait<&'a Self> for UPolynomial<'a, R> {
+            type Output = Self;
+            fn $method_name(self, other: &'a Self) -> Self {
+                let other = other.clone();
+                $trait::$method_name(self, other)
+            }
+        }
+    };
+}
+macro_rules! ref_assign_bin_op {
+    ($trait:tt, $method_name:ident, $underlying_op_name:path) => {
+        impl<'a, R: Ring<'a>> $trait<&'a Self> for UPolynomial<'a, R> {
+            fn $method_name(&mut self, other: &'a Self) {
+                let other = other.clone();
+                *self = $underlying_op_name(self.clone(), other);
+            }
+        }
+    };
 }
 
-impl<'a, R: Ring<'a>> Sub<&'a Self> for UPolynomial<'a, R> {
-    type Output = Self;
+ref_bin_op!(Add, add);
+ref_bin_op!(Sub, sub);
+ref_bin_op!(Mul, mul);
+ref_assign_bin_op!(AddAssign, add_assign, Add::add);
+ref_assign_bin_op!(SubAssign, sub_assign, Sub::sub);
+ref_assign_bin_op!(MulAssign, mul_assign, Mul::mul);
 
-    fn sub(self, other: &'a Self) -> Self {
-        let other = other.clone();
-        self - other
-    }
-}
-
-impl<'a, R: Ring<'a>> Mul<&'a Self> for UPolynomial<'a, R> {
-    type Output = Self;
-
-    fn mul(self, other: &'a Self) -> Self {
-        let other = other.clone();
-        self * other
-    }
-}
-/*
 impl<'a, R: Ring<'a>> Ring<'a> for UPolynomial<'a, R> {}
 
 impl<'a, R: Ring<'a>> Polynomial<'a> for UPolynomial<'a, R> {
@@ -159,7 +185,6 @@ impl<'a, R: Ring<'a>> Polynomial<'a> for UPolynomial<'a, R> {
         1
     }
 }
-*/
 
 /// TESTS
 #[cfg(test)]
@@ -188,5 +213,31 @@ mod tests {
         let q = UPolynomial::new(vec![4, 9, 4, 3]);
         let expected = UPolynomial::new(vec![8, 6, 1, 39, 11, 15]);
         assert_eq!(p * q, expected);
+    }
+    #[test]
+    fn test_add_assign() {
+        let mut p = UPolynomial::new(vec![2, -3, 5]);
+        let q = UPolynomial::new(vec![4, 9, 4, 3]);
+        p += q;
+        let expected = UPolynomial::new(vec![6, 6, 9, 3]);
+        assert_eq!(p, expected);
+    }
+
+    #[test]
+    fn test_sub_assign() {
+        let mut p = UPolynomial::new(vec![2, -3, 5]);
+        let q = UPolynomial::new(vec![4, 9, 4, 3]);
+        p -= q;
+        let expected = UPolynomial::new(vec![-2, -12, 1, -3]);
+        assert_eq!(p, expected);
+    }
+
+    #[test]
+    fn test_mul_assign() {
+        let mut p = UPolynomial::new(vec![2, -3, 5]);
+        let q = UPolynomial::new(vec![4, 9, 4, 3]);
+        p *= q;
+        let expected = UPolynomial::new(vec![8, 6, 1, 39, 11, 15]);
+        assert_eq!(p, expected);
     }
 }
